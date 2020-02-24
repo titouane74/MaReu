593535ca4df1c0b65f9a166fdb900be80bc74ef2
titouane74 <57191853+titouane74@users.noreply.github.com>
2020-02-24 17:55:17 +0100

    Affiche des menus sort et filter sur 2 icones différents

--- app/src/main/java/com/ocr/mareu/utils/ShowDialogBox.java
+++ app/src/main/java/com/ocr/mareu/utils/ShowDialogBox.java
@@ -1,109 +1,25 @@
 package com.ocr.mareu.utils;
 
-import android.app.AlertDialog;
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.os.Bundle;
 import android.widget.DatePicker;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
-
 import com.ocr.mareu.R;
-import com.ocr.mareu.model.Room;
 import com.ocr.mareu.ui.fragments.ListFragment;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 /**
  * Created by Florence LE BOURNOT on 07/02/2020
  */
-public class ShowDialogFragRooms extends DialogFragment {
-
-    /**
-     * Affichage de la boîte de dialogue contenant les salles de réunion pour l'application du filtre par salle
-     */
-    /**
-     * Override to build your own custom Dialog container.  This is typically
-     * used to show an AlertDialog instead of a generic Dialog; when doing so,
-     * {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)} does not need
-     * to be implemented since the AlertDialog takes care of its own content.
-     *
-     * <p>This method will be called after {@link #onCreate(Bundle)} and
-     * before {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.  The
-     * default implementation simply instantiates and returns a {@link Dialog}
-     * class.
-     *
-     * <p><em>Note: DialogFragment own the {@link Dialog#setOnCancelListener
-     * Dialog.setOnCancelListener} and {@link Dialog#setOnDismissListener
-     * Dialog.setOnDismissListener} callbacks.  You must not set them yourself.</em>
-     * To find out about these events, override {@link #onCancel(DialogInterface)}
-     * and {@link #onDismiss(DialogInterface)}.</p>
-     *
-     * @param savedInstanceState The last saved instance state of the Fragment,
-     *                           or null if this is a freshly created Fragment.
-     * @return Return a new Dialog instance to be displayed by the Fragment.
-     */
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
-    }
-
-    public static List<Room> showDialogRooms(Context pContext, ListFragment pListFragment, List<Room> pRooms) {
-
-        AlertDialog.Builder lBuilder = new AlertDialog.Builder(pContext);
-        lBuilder.setTitle(R.string.meeting_room_selected);
-
-        List<Room> lRoomsSelected = new ArrayList<>();
-        List<String> lListRooms = new ArrayList<>();
-
-        for (Room lRoom : pRooms) {
-            lListRooms.add(lRoom.getNameRoom());
-        }
-
-        final CharSequence[] charSequenceItems = lListRooms.toArray(new CharSequence[lListRooms.size()]);
-
-
-        lBuilder.setMultiChoiceItems(charSequenceItems, null, new DialogInterface.OnMultiChoiceClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                if (isChecked) {
-                    lRoomsSelected.add(pRooms.get(which));
-                }
-            }
-        });
-
-
-        lBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) { }
-        });
-        lBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                pListFragment.listToUpdate(SortOrFilterLabel.FILTER_ROOM);
-            }
-        });
-
-
-        Dialog lDialog = lBuilder.create();
-
-        lDialog.show();
-
-        return lRoomsSelected;
-    }
+public class ShowDialogBox  {
 
     /**
      * Affichage du calendrier pour l'application du filtre par date
      * @param pContext
      */
+
     public static Calendar showCalendarDialog(Context pContext, ListFragment pListFragment) {
 
         Calendar lCalendar = Calendar.getInstance();
